d9ebc204e13c82f71b062133d0a603910b9f0bdf
chase kirk <kirklubesit30908635@gmail.com>
2026-02-01 08:36:31 -0500

    Add root operator console page

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+﻿export default function Home() {
+  return (
+    <main style={{ padding: 32 }}>
+      <h1>Autokirk Operator Console</h1>
+      <p>Build verified. Next.js is running.</p>
+    </main>
+  );
+}
