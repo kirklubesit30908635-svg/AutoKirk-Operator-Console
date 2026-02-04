6c8904296ca17fe19133b5046324a928affccdf3
chase kirk <kirklubesit30908635@gmail.com>
2026-02-04 16:50:09 -0500

    Update supabaseServer.ts

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -1,22 +1,24 @@
 ﻿import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 
-export async function createSupabaseServer() {
+export function createSupabaseServer() {
+  const cookieStore = cookies();
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
     {
       cookies: {
-        get(name) {
-          return cookies().get(name)?.value;
+        get(name: string) {
+          return cookieStore.get(name)?.value;
         },
-        set(name, value, options) {
-          cookies().set({ name, value, ...options });
+        set(name: string, value: string, options: any) {
+          cookieStore.set({ name, value, ...options });
         },
-        remove(name, options) {
-          cookies().set({ name, value: "", ...options });
-        }
-      }
+        remove(name: string, options: any) {
+          cookieStore.set({ name, value: "", ...options });
+        },
+      },
     }
   );
 }
